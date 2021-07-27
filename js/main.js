c3e8de218047e85eed18d9c39e3da691c3cc97bd
mrsk613 <74517898+mrsk613@users.noreply.github.com>
2021-07-27 22:36:11 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,17 +40,17 @@ const slideShow = function () {
 slideShow();
 
 //更新情報
- $(function() {
-  	$.ajax({
-  		url: 'news/index.html' //読み込むファイルを指定
-  	})
-    .then(
-      // 通信成功時の処理
-      function(html) {
-        $(html).find('#news-area').each(function(){　//外部HTMLの読み込みたい部分を指定
-  				$('#top-info').html($(this).html()); //読み込んだ外部HTMLを表示する領域を指定
-  				return false;
-  			});
-  			$('dl dt:gt(4), dl dd:gt(4)').remove(); //5件目以降の dt,dd を削除
-      });
-  });
+$(function() {
+	$.ajax({
+		url: 'news/index.html' //読み込むファイルを指定
+	})
+	.then(
+		// 通信成功時の処理
+		function(html) {
+			$(html).find('#news-area').each(function(){　//外部HTMLの読み込みたい部分を指定
+				$('#top-info').html($(this).html()); //読み込んだ外部HTMLを表示するエリアを指定
+				return false;
+			});
+			$('dl dt:gt(4), dl dd:gt(4)').remove(); //2件目以降の dt,dd を削除
+		});
+});
